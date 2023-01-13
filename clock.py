d5080aa92254713b799686da1c03a663856e7032
Aditha Dinuja Serasinghe <58882366+S-V-A-D-Serasinghe@users.noreply.github.com>
2023-01-13 22:50:43 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ def time():
     string=strftime('%H:%M:%S %p')
     Label.config(text=string)
     Label.after(1000,time)
-Label = Label(root, font=('ds-digital',80),background="black", foreground="white")
+Label = Label(root, font=('DS-Digital',80),background="black", foreground="white")
 Label.pack(anchor='center')
 time()
 mainloop()
